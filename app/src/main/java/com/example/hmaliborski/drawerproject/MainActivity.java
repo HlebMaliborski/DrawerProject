93eff53b65cf79fa9db05dd7c08d66ec3aeec52b
Test name <hmaliborski@deloitte.com>
2017-04-21 13:04:01 +0300

    //Test commit

--- app/src/main/java/com/example/hmaliborski/drawerproject/MainActivity.java
+++ app/src/main/java/com/example/hmaliborski/drawerproject/MainActivity.java
@@ -49,8 +49,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-
         isStoragePermissionGranted();
 
         titles = new String[]{"Picasso assets images", "Picasso file system images", "Picasso internet images",
